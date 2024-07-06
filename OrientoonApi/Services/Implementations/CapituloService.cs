b83cf2d26383ffed38be3badaf20c0c767165f72
lucasolopes <olopes.lucas567@gmail.com>
2024-07-06 20:50:06 -0300

    Implementando metodos na service Capitulo

--- OrientoonApi/Services/Implementations/CapituloService.cs
+++ OrientoonApi/Services/Implementations/CapituloService.cs
@@ -1,8 +1,58 @@
-﻿using OrientoonApi.Services.Interfaces;
+﻿using OrientoonApi.Data.Repositories.Interfaces;
+using OrientoonApi.Models.Entities;
+using OrientoonApi.Services.Interfaces;
 
 namespace OrientoonApi.Services.Implementations
 {
     public class CapituloService : ICapituloService
     {
+        private readonly ICapituloRepository _capituloRepository;
+        private readonly IImagemRepository _imagemRepository;
+        private readonly IConfiguration _configuration;
+
+
+
+        public CapituloService(ICapituloRepository capituloRepository, IImagemRepository imagemRepository, IConfiguration configuration)
+        {
+            _capituloRepository = capituloRepository;
+            _imagemRepository = imagemRepository;
+            _configuration = configuration;
+        }
+
+        public async Task<CapituloModel> AddCapituloAsync(string mangaId, double numCap, IList<IFormFile> files)
+        {
+             var uploadPath = _configuration["FileUploadPath"];
+            var capitulo = new CapituloModel
+            {
+                NumCapitulo = numCap,
+                Id = mangaId,
+                Caminho = Path.Combine(uploadPath, mangaId, numCap.ToString())
+            };
+
+            Directory.CreateDirectory(capitulo.Caminho);
+            int ordem = 1;
+            foreach(var file in files)
+            {
+                var imagem = new ImagemModel
+                {
+                    CapituloId = capitulo.Id,
+                    Ordem = ordem,
+                    Caminho = Path.Combine(capitulo.Caminho, file.FileName)
+                };
+                ordem++;
+                using (var stream = new FileStream(imagem.Caminho, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                await _imagemRepository.AddAsync(imagem);
+            }
+            return await _capituloRepository.AddAsync(capitulo);
+
+        }
+
+        public async Task<CapituloModel> GetCapituloByIdAsync(string id)
+        {
+            return await _capituloRepository.GetByIdAsync(id);
+        }
     }
 }
--- OrientoonApi/Services/Interfaces/ICapituloService.cs
+++ OrientoonApi/Services/Interfaces/ICapituloService.cs
@@ -1,6 +1,10 @@
-﻿namespace OrientoonApi.Services.Interfaces
+﻿using OrientoonApi.Models.Entities;
+
+namespace OrientoonApi.Services.Interfaces
 {
     public interface ICapituloService
     {
+        Task<CapituloModel> AddCapituloAsync(string mangaId, double numCap, IList<IFormFile> files);
+        Task<CapituloModel> GetCapituloByIdAsync(string id);
     }
 }
