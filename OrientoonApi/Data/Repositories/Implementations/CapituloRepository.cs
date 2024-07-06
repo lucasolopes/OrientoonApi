04aacf49ea6441b7a08353ad7e82c8e256b95b1b
lucasolopes <olopes.lucas567@gmail.com>
2024-07-06 20:23:04 -0300

    Criando Repository de capitulo

---
+++ OrientoonApi/Data/Repositories/Implementations/CapituloRepository.cs
@@ -0,0 +1,26 @@
+﻿using OrientoonApi.Data.Contexts;
+using OrientoonApi.Data.Repositories.Interfaces;
+using OrientoonApi.Models.Entities;
+
+namespace OrientoonApi.Data.Repositories.Implementations
+{
+    public class CapituloRepository : ICapituloRepository
+    {
+        private readonly OrientoonContext _context;
+
+        public CapituloRepository(OrientoonContext context)
+        {
+            _context = context;
+        }
+
+        public Task<CapituloModel> AddCapituloAsync(CapituloModel capitulo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CapituloModel> GetCapituloByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OrientoonApi/Data/Repositories/Interfaces/ICapituloRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrientoonApi.Models.Entities;
+
+namespace OrientoonApi.Data.Repositories.Interfaces
+{
+    public interface ICapituloRepository 
+    {
+        Task<CapituloModel> AddCapituloAsync(CapituloModel capitulo);
+        Task<CapituloModel> GetCapituloByIdAsync(string id);
+
+    }
+}
