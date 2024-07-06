981657044fcbbe87da9c16b34f0a05f45c297d78
lucasolopes <olopes.lucas567@gmail.com>
2024-07-06 20:48:31 -0300

    Criando repository de imagem

---
+++ OrientoonApi/Data/Repositories/Implementations/ImagemRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrientoonApi.Data.Contexts;
+using OrientoonApi.Data.Repositories.Interfaces;
+using OrientoonApi.Models.Entities;
+
+namespace OrientoonApi.Data.Repositories.Implementations
+{
+    public class ImagemRepository : IImagemRepository
+    {
+
+        private readonly OrientoonContext _context;
+
+        public ImagemRepository(OrientoonContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(ImagemModel imagem)
+        {
+            await _context.Imagem.AddAsync(imagem);
+        }
+
+        public Task<List<ImagemModel>> GetByCapituloIdAsync(string capituloId)
+        {
+            return _context.Imagem.Where(x => x.CapituloId == capituloId).ToListAsync();
+        }
+    }
+}
---
+++ OrientoonApi/Data/Repositories/Interfaces/IImagemRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrientoonApi.Models.Entities;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OrientoonApi.Data.Repositories.Interfaces
+{
+    public interface IImagemRepository
+    {
+        Task AddAsync(ImagemModel imagem);
+        Task<List<ImagemModel>> GetByCapituloIdAsync(string capituloId);
+    }
+}
