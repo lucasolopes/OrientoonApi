d80f078d6e0e998b8aac5cecb7d444b1bf26ac71
lucasolopes <olopes.lucas567@gmail.com>
2024-07-06 20:50:26 -0300

    criando endpoints na controller capitulo

--- OrientoonApi/Controllers/CapituloController.cs
+++ OrientoonApi/Controllers/CapituloController.cs
@@ -12,5 +12,30 @@ namespace OrientoonApi.Controllers
         {
             _capituloService = capituloService;
         }
+
+        [HttpPost("{orientoonId}")]
+        public async Task<IActionResult> UploadCapitulo(string orientoonId, [FromForm] double numCap, [FromForm] IList<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                return BadRequest("No files provided.");
+            }
+
+            var capitulo = await _capituloService.AddCapituloAsync(orientoonId, numCap, files);
+            return Ok(capitulo);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCapitulo(string id)
+        {
+            var capitulo = await _capituloService.GetCapituloByIdAsync(id);
+            if (capitulo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(capitulo);
+        }
     }
 }
