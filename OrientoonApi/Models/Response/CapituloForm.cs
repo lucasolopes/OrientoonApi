abdd0f69f84ea0ded26c08ef44f1adc16e2a30fa
lucasolopes <olopes.lucas567@gmail.com>
2024-07-07 09:44:37 -0300

    criando form para capitulo e imagem

---
+++ OrientoonApi/Models/Response/CapituloForm.cs
@@ -0,0 +1,20 @@
+﻿using OrientoonApi.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrientoonApi.Models.Response
+{
+    public class CapituloForm
+    {
+        public string Id;
+
+        public Double NumCapitulo;
+
+        public DateOnly DataLancamento;
+
+        public Double AvaliacaoCap;
+
+        public string OrientoonId;
+
+        public List<ImagemForm> Imagens;
+    }
+}
---
+++ OrientoonApi/Models/Response/ImagemForm.cs
@@ -0,0 +1,15 @@
+﻿using OrientoonApi.Models.Entities;
+
+namespace OrientoonApi.Models.Response
+{
+    public class ImagemForm
+    {
+        public string Id;
+
+        public int Ordem;
+
+        public string CapituloId;
+
+        public string Caminho;
+    }
+}
