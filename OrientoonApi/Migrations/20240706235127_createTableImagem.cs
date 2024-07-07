50529218677273c3dc40d7c18d71083f0738b572
lucasolopes <olopes.lucas567@gmail.com>
2024-07-07 09:42:50 -0300

    subindo banco

---
+++ OrientoonApi/Migrations/20240706235127_createTableImagem.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrientoonApi.Migrations
+{
+    public partial class createTableImagem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Capitulo");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "NumCapitulo",
+                table: "Capitulo",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Imagem",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NomeArquivo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ordem = table.Column<int>(type: "int", nullable: false),
+                    CapituloId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Caminho = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Imagem_Capitulo_CapituloId",
+                        column: x => x.CapituloId,
+                        principalTable: "Capitulo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagem_CapituloId",
+                table: "Imagem",
+                column: "CapituloId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imagem");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumCapitulo",
+                table: "Capitulo",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Capitulo",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
