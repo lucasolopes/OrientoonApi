506e5e7925671fde29b355b4ea9271a6ed1bda0d
lucasolopes <olopes.lucas567@gmail.com>
2024-07-12 10:41:31 -0300

    Criando Enum de Status

---
+++ OrientoonApi/Enum/StatusEnum.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace OrientoonApi.Enum
+{
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum StatusEnum
+    {
+        [EnumMember(Value = "EmAndamento")]
+        EmAndamento,
+        [EnumMember(Value = "Concluido")]
+        Concluido,
+        [EnumMember(Value = "Cancelado")]
+        Cancelado,
+        [EnumMember(Value = "Hiato")]
+        Hiato
+    }
+}
