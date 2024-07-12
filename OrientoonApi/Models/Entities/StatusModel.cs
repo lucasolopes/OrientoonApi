3d396840e0d5d87f8af942537ebb5dc98d6708bc
lucasolopes <olopes.lucas567@gmail.com>
2024-07-12 10:45:00 -0300

    Alterando Status string para enum na model

--- OrientoonApi/Models/Entities/StatusModel.cs
+++ OrientoonApi/Models/Entities/StatusModel.cs
@@ -1,4 +1,5 @@
-﻿using OrientoonApi.Models.Response;
+﻿using OrientoonApi.Enum;
+using OrientoonApi.Models.Response;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
@@ -8,15 +9,27 @@ namespace OrientoonApi.Models.Entities
     [Table("Status")]
     public class StatusModel
     {
+
+        public StatusModel()
+        {
+        }
+
+        public StatusModel(StatusEnum? status)
+        {
+            if (status != null)
+                nome = status.GetValueOrDefault();
+        }
+
         [Key]
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
 
         [Column("nome",TypeName = "varchar(50)")]
         [MaxLength(50)]
         [MinLength(1)]
         [DataType(DataType.Text)]
         [StringLength(50)]
-        public string nome { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public StatusEnum nome { get; set; }
 
         [JsonIgnore]
         public ICollection<OrientoonModel> Orientoons { get; set; }
