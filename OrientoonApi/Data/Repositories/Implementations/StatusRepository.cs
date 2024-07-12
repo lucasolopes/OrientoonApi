9417f7d95490f5510da27c4895687ba111132e2f
lucasolopes <olopes.lucas567@gmail.com>
2024-07-12 11:13:51 -0300

    Criando repository de status

--- OrientoonApi/Data/Repositories/Implementations/StatusRepository.cs
+++ OrientoonApi/Data/Repositories/Implementations/StatusRepository.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OrientoonApi.Data.Contexts;
 using OrientoonApi.Data.Repositories.Interfaces;
+using OrientoonApi.Enum;
 using OrientoonApi.Models.Entities;
 
 namespace OrientoonApi.Data.Repositories.Implementations
 {
-    public class StatusRepository : GenericRepository<StatusModel>, IStatusRepository
+    public class StatusRepository : IStatusRepository
     {
-        public StatusRepository(OrientoonContext context) : base(context)
+        private readonly OrientoonContext _context;
+        public StatusRepository(OrientoonContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<StatusModel>> GetAllAsync()
+        {
+            return await _context.Status.ToListAsync();
+        }
+
+        public async Task<StatusModel> GetByIdAsync(string statusId)
+        {
+            return await _context.Status.FirstOrDefaultAsync(x => x.Id == statusId);
+        }
+
+        public async Task<bool> ExistsByIdAsync(string statusId)
+        {
+            return await _context.Status.AnyAsync(x => x.Id == statusId);
+        }
+
+        public async Task<StatusModel> GetByNameAsync(StatusEnum? status)
+        {
+            return await _context.Status.FirstOrDefaultAsync(x => x.nome == status);
+        }
+
+        public Task<bool> ExistsByNameAsync(StatusEnum? status)
+        {
+            return Task.FromResult(_context.Status.Any(x => x.nome == status));
         }
     }
 }
--- OrientoonApi/Data/Repositories/Interfaces/IStatusRepository.cs
+++ OrientoonApi/Data/Repositories/Interfaces/IStatusRepository.cs
@@ -1,8 +1,14 @@
-﻿using OrientoonApi.Models.Entities;
+﻿using OrientoonApi.Enum;
+using OrientoonApi.Models.Entities;
 
 namespace OrientoonApi.Data.Repositories.Interfaces
 {
-    public interface IStatusRepository : IGenericRepository<StatusModel>
+    public interface IStatusRepository
     {
+        Task<StatusModel> GetByIdAsync(string statusId);
+        Task<IEnumerable<StatusModel>> GetAllAsync();
+        Task<bool> ExistsByIdAsync(string statusId);
+        Task<StatusModel> GetByNameAsync(StatusEnum? status);
+        Task<bool> ExistsByNameAsync(StatusEnum? status);
     }
 }
