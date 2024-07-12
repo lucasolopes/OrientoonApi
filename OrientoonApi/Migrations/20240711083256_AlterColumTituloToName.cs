12407b21efb7bb08fdded827aa88e061ce11ad4b
lucasolopes <olopes.lucas567@gmail.com>
2024-07-12 11:11:12 -0300

    Atualizando Banco

---
+++ OrientoonApi/Migrations/20240711083256_AlterColumTituloToName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrientoonApi.Migrations
+{
+    public partial class AlterColumTituloToName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NormalizedTitulo",
+                table: "Orientoon",
+                newName: "NormalizedName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NormalizedName",
+                table: "Orientoon",
+                newName: "NormalizedTitulo");
+        }
+    }
+}
